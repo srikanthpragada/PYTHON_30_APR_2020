5cd45f98d356644fe30aa003bae2396dc2893f76
srikanthpragada <srikanthpragada@gmail.com>
2020-05-02 09:06:53 +0530

    02-MAY

---
+++ demo/even_odd.py
@@ -0,0 +1,9 @@
+# Program to display whether a number is even or odd
+
+num = int(input("Enter a number :"))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+print('The End')
---
+++ demo/greet.py
@@ -0,0 +1,5 @@
+
+# Program to greet user
+
+name = input("Enter your name :")
+print("Hello", name)
